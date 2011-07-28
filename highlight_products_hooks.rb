16d50ef9235865fd1c8987ec02e9e7eef29300c8
Fabien Franzen <info@atelierfabien.be>
2011-07-28 15:39:53 +0200

    Removed obsolete files

--- highlight_products_hooks.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(
-  :virtual_path => "admin/products/index",
-  :insert_bottom => "[data-hook='admin_products_index_row_actions']",
-  :partial => "admin/products/highlight_product_button",
-  :disabled => false)
