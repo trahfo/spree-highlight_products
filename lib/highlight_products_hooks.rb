db8c0d1683102f5be36a7e60d22e38c76bb732b2
Fabien Franzen <info@atelierfabien.be>
2011-10-17 12:02:52 +0200

    Added :name to hooks

--- lib/highlight_products_hooks.rb
+++ lib/highlight_products_hooks.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(
+  :name => 'highlight_row_actions',
   :virtual_path => "admin/products/index",
   :insert_bottom => "[data-hook='admin_products_index_row_actions']",
   :partial => "admin/products/highlight_product_button",
